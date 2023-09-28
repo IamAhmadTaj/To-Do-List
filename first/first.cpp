a21288a7e8925689d2b08cf45557db0c7d150ce1
TayabTaj <136854650+TayabTaj@users.noreply.github.com>
2023-09-28 12:44:20 -0700

    Completed

--- first/first.cpp
+++ first/first.cpp
@@ -1,56 +1,74 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-    vector<string> todoList;
-
+    string todoList;
+    string task;
     char choice;
 
-    cout << "Simple To-Do List Program" << endl;
+    cout << "Simple To-Do List" << endl;
 
-    do {
-        cout << "\nMenu:\n";
-        cout << "1. Add Task\n";
-        cout << "2. Display To-Do List\n";
-        cout << "3. Quit\n";
-        cout << "Enter your choice (1/2/3): ";
+    while (true) {
+        cout << "\nMenu:" << endl;
+        cout << "1. Add Task" << endl;
+        cout << "2. List Tasks" << endl;
+        cout << "3. Remove Task" << endl;
+        cout << "4. Quit" << endl;
+        cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
         case '1':
-        {
-            string task;
-            cout << "Enter the task to add: ";
-           // cin.ignore();  // Clear the input buffer
-            //cin.getline(cin, task);
-            cin >> task;
-            todoList.push_back(task);
-            cout << "Task added successfully." << endl;
+            cout << "Enter a task: ";
+            cin.ignore(); // Clear newline character from previous input
+            getline(cin, task);
+            todoList += task + "\n";
+            cout << "Task added!" << endl;
             break;
-        }
         case '2':
-        {
             if (todoList.empty()) {
                 cout << "Your to-do list is empty." << endl;
             }
             else {
                 cout << "To-Do List:" << endl;
-                for (size_t i = 0; i < todoList.size(); ++i) {
-                    cout << i + 1 << ". " << todoList[i] << endl;
-                }
+                cout << todoList;
             }
             break;
-        }
         case '3':
-            cout << "Goodbye!" << endl;
+            if (todoList.empty()) {
+                cout << "Your to-do list is already empty." << endl;
+            }
+            else {
+                int taskNum;
+                cout << "Enter the task number to remove: ";
+                cin >> taskNum;
+
+                istringstream iss(todoList);   //here i made a sstream . and intialize it with the content of todolist. 
+                                               // Note : its alow us to split our todolist into lines...
+                string line;
+                string updatedList;
+
+                for (int i = 1; getline(iss, line); i++) {
+                    if (i != taskNum) {
+                        updatedList += line + "\n";
+                    }
+                }
+
+                todoList = updatedList;
+                cout << "Task removed!" << endl;
+            }
             break;
+        case '4':
+            cout << "Goodbye!" << endl;
+            return 0;
         default:
             cout << "Invalid choice. Please try again." << endl;
             break;
         }
-    } while (choice != '3');
+    }
 
     return 0;
 }
